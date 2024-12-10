26b7b5c480241b2c73d6e327c7f7b1a7683ca6b2
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-12-10 10:38:14 +0530

    Time: 301 ms (7.14%) | Memory: 12.3 MB (5.88%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maximumLength(self, s):
+        n = len(s)
+        mp = {}
+        for i in range(n):
+            for j in range(i, n):
+                substring = s[i:j + 1]
+                unique_chars = set(substring)
+                if len(unique_chars) == 1:
+                    mp[substring] = mp.get(substring, 0) + 1
+
+        maxi = -1
+        for key, value in mp.items():
+            if value >= 3:
+                maxi = max(maxi, len(key))
+
+        return maxi
+        
